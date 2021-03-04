c958279d9d0b2d0796069ae3ce63cf786bd6b8ac
ZakariaAitErrami <zakaria.ait.errami00@gmail.com>
2021-03-04 23:17:21 +0100

    Changing login

--- app/src/main/java/com/example/managementsystem/MainActivity.java
+++ app/src/main/java/com/example/managementsystem/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
@@ -74,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
     public void display(){
         Intent teacher = new Intent(this, drawer_activity.class);
